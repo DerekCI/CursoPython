c7f8e7bd08baeda49c2151536e43aab5323f0d7e
Derek <derekcortez16@gmail.com>
2019-05-14 15:53:40 -0500

    clase 2

--- arbolflores.py
+++ arbolflores.py
@@ -10,6 +10,12 @@ import numpy
 
 iris = load_iris()
 
+prueba_target = iris.target[50]
+prueba_data = iris.data[50]
+
 #Eliminando los 3 datos del data set 0, 50 y 100
 target = numpy.delete(iris.target,[0,50,100])
-data = numpy.delete(iris.data,[0,50,100])
+data = numpy.delete(iris.data,[0,50,100],axis=0) #Axis = 0 son renglones
+
+
+clasificador = tree.DecisionTreeClassifier()
