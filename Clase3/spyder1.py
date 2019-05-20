e40b9cdbdd3f9abad2b708dfdcd10221dcf861c9
Derek <derekcortez16@gmail.com>
2019-05-20 14:33:32 -0500

    clase3

---
+++ Clase3/spyder1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 20 14:30:08 2019
+
+@author: derekcortez
+"""
+
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
